092f0fd45d2adfa8576bc06804ece6abdcf2b2c2
fred <fred@PC-TERRA.home>
2023-10-10 19:02:22 +0200

    still not ok (3 versions of getting path implemented & tested)

--- src/main/java/plugins/fmp/multicafe2/resource/ResourceUtilFMP.java
+++ src/main/java/plugins/fmp/multicafe2/resource/ResourceUtilFMP.java
@@ -9,7 +9,6 @@ import plugins.fmp.multicafe2.MultiCAFE2;
 
 
 
-
 // adapted from NherveToolbox
 
 
@@ -17,7 +16,8 @@ public class ResourceUtilFMP {
 
     public static final String ALPHA_ICON_PATH 	= "alpha/";
     public static final String ICON_PATH 		= "icon/";
-    public static final String RESOURCES_PATH 	= "../../../";
+    public static final String RESOURCES_PATH   = "resources/";
+    public static final String ROOT_PATH 		= "../../../";
     
     public static final IcyIcon ICON_FIT_YAXIS  = getIcyIcon("fit_y.png");
     public static final IcyIcon ICON_FIT_XAXIS  = getIcyIcon("fit_x.png");
@@ -25,11 +25,7 @@ public class ResourceUtilFMP {
     
     private static IcyIcon getIcyIcon(String fileName) 
     {
-    	String name = 
-    			ICON_PATH + 
-    			ALPHA_ICON_PATH + 
-    			fileName;
-		return new IcyIcon(getImage(name));
+		return new IcyIcon(getImage(fileName));
 	}
     
 	private static Image getImage(String fileName) 
@@ -39,10 +35,22 @@ public class ResourceUtilFMP {
 //		pkg += RESOURCES_PATH+ fileName;
 //		System.out.println(pkg);
 		
-		String name = fileName;
+		String name = ICON_PATH + ALPHA_ICON_PATH + fileName;
 		InputStream url = MultiCAFE2.class.getClassLoader().getResourceAsStream(name);
-		if (url == null)
-			System.out.println("resource not found: "+ name);
+		if (url == null) {
+			System.out.println("1 - resource not found: at: "+ name);
+			String name1 = RESOURCES_PATH + name;
+			url = MultiCAFE2.class.getClassLoader().getResourceAsStream(name1);
+			if (url == null) 
+			{			
+				System.out.println("2 - resource not found either at: "+ name1);
+				String name2 = ROOT_PATH + name;
+				url = MultiCAFE2.class.getClassLoader().getResourceAsStream(name2);
+				if (url == null) 
+					System.out.println("3 - resource not found either at: "+ name2);
+			}
+		}
 		return ImageUtil.load(url);
 	}
+	
 }
