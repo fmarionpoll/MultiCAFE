7327a308522c9ffbad2a34448caf87dc00e6e2dd
Frederic Marion-Poll <marion_poll@hotmail.com>
2023-10-13 18:10:46 +0200

    move resources into a directory with the same name as the plugin

--- src/main/java/plugins/fmp/multicafe2/resource/ResourceUtilFMP.java
+++ src/main/java/plugins/fmp/multicafe2/resource/ResourceUtilFMP.java
@@ -16,15 +16,13 @@ public class ResourceUtilFMP {
 
     public static final String ALPHA_PATH 		= "alpha/";
     public static final String ICON_PATH 		= "icon/";
-//    public static final String RESOURCES_PATH   = "resources/";
-//    public static final String ROOT_PATH 		= "../../../";
+
     
     public static final IcyIcon ICON_PREVIOUS_IMAGE 	= getIcyIcon("br_prev.png");
     public static final IcyIcon ICON_NEXT_IMAGE  		= getIcyIcon("br_next.png");
     public static final IcyIcon ICON_FIT_YAXIS  		= getIcyIcon("fit_Y.png");
     public static final IcyIcon ICON_FIT_XAXIS  		= getIcyIcon("fit_X.png");
-//    public static final IcyIcon ALT_ICON_FIT_YAXIS  	= getIcyIcon("cursor_H_split.png");
-//    public static final IcyIcon ALT_ICON_FIT_XAXIS  	= getIcyIcon("cursor_V_split.png");
+
     
 
   
@@ -39,7 +37,7 @@ public class ResourceUtilFMP {
 		if (img != null)
 			return img;
 		
-		String name = ICON_PATH + ALPHA_PATH + fileName;
+		String name = "plugins/fmp/multicafe2/" + ICON_PATH + ALPHA_PATH + fileName;
 		InputStream url = MultiCAFE2.class.getClassLoader().getResourceAsStream(name);
 		if (url == null) {
 			System.out.println("ResourceUtilFMP:getImage resource not found: at: "+ name);
