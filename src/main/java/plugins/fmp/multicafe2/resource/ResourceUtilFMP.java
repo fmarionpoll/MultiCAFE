09cb8e36fc226014ca707b16edd22f31e9639a55
Frederic Marion-Poll <marion_poll@hotmail.com>
2023-10-13 15:48:43 +0200

    comment 2 lines referring to icy icons

--- src/main/java/plugins/fmp/multicafe2/resource/ResourceUtilFMP.java
+++ src/main/java/plugins/fmp/multicafe2/resource/ResourceUtilFMP.java
@@ -23,8 +23,8 @@ public class ResourceUtilFMP {
     public static final IcyIcon ICON_NEXT_IMAGE  		= getIcyIcon("br_next.png");
     public static final IcyIcon ICON_FIT_YAXIS  		= getIcyIcon("fit_Y.png");
     public static final IcyIcon ICON_FIT_XAXIS  		= getIcyIcon("fit_X.png");
-    public static final IcyIcon ALT_ICON_FIT_YAXIS  	= getIcyIcon("cursor_H_split.png");
-    public static final IcyIcon ALT_ICON_FIT_XAXIS  	= getIcyIcon("cursor_V_split.png");
+//    public static final IcyIcon ALT_ICON_FIT_YAXIS  	= getIcyIcon("cursor_H_split.png");
+//    public static final IcyIcon ALT_ICON_FIT_XAXIS  	= getIcyIcon("cursor_V_split.png");
     
 
   
