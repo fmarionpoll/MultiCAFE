c7f1d0310a0f7a895591974129d0be0cf5befc7f
fred <fred@PC-TERRA.home>
2023-10-15 19:18:27 +0200

    update chart levels (use cobinedrangeXYPlot)

--- src/main/java/plugins/fmp/multicafe2/tools/chart/ChartLevels.java
+++ src/main/java/plugins/fmp/multicafe2/tools/chart/ChartLevels.java
@@ -1,6 +1,8 @@
 package plugins.fmp.multicafe2.tools.chart;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Paint;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
@@ -10,31 +12,32 @@ import java.util.List;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
-import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartColor;
 import org.jfree.chart.ChartMouseEvent;
 import org.jfree.chart.ChartMouseListener;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.entity.ChartEntity;
-import org.jfree.chart.entity.XYItemEntity;
-import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.CombinedRangeXYPlot;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
 import icy.gui.frame.IcyFrame;
 import icy.gui.util.GuiUtil;
 import icy.gui.viewer.Viewer;
+
 import plugins.fmp.multicafe2.MultiCAFE2;
 import plugins.fmp.multicafe2.experiment.Experiment;
-
 import plugins.fmp.multicafe2.tools.toExcel.EnumXLSExportType;
 import plugins.fmp.multicafe2.tools.toExcel.XLSExport;
 import plugins.fmp.multicafe2.tools.toExcel.XLSExportOptions;
 import plugins.fmp.multicafe2.tools.toExcel.XLSResults;
 import plugins.fmp.multicafe2.tools.toExcel.XLSResultsArray;
 
-public class ChartLevels extends IcyFrame  
+public class ChartLevels extends IcyFrame
 {
 	public JPanel 	mainChartPanel 	= null;
 	public IcyFrame mainChartFrame 	= null;
@@ -44,7 +47,6 @@ public class ChartLevels extends IcyFrame
 	private boolean flagMaxMinSet = false;
 	private double globalYMax = 0;
 	private double globalYMin = 0;
-	private double globalXMin = 0;
 	private double globalXMax = 0;
 
 	private double ymax = 0;
@@ -75,54 +77,60 @@ public class ChartLevels extends IcyFrame
 	public void displayData(Experiment exp, EnumXLSExportType option, boolean subtractEvaporation) 
 	{
 		xyChartList.clear();
-
+		
+		Dimension dim = this.getSize();
+		System.out.println(dim);
+		
 		ymax = 0;
 		ymin = 0;
-		List<XYSeriesCollection> xyDataSetList = new ArrayList <XYSeriesCollection>();
 		flagMaxMinSet = false;
-		getDataArrays(exp, option, subtractEvaporation, xyDataSetList);
+		List<XYSeriesCollection> xyDataSetList = getDataArrays(exp, option, subtractEvaporation);;
 		
-		// display charts
-		int width = 140; 
-		int minimumDrawWidth = 100;
-		int maximumDrawWidth = width;
-		int height = 200;
-		int maximumDrawHeight = height;
+		int icage = 0;
 		
-		boolean displayLabels = true;
-		String yTitle = option.toUnit();
-		int ichart = 0;
+		// make a common vertical axis for all the sub-plots  
+        final NumberAxis valueAxis = new NumberAxis("liquid level (µl)");
+        valueAxis.setAutoRangeIncludesZero(false);  
+        valueAxis.setInverted(true);
+        final CombinedRangeXYPlot parent = new CombinedRangeXYPlot(valueAxis);
+        
+        Paint[] color = ChartColor.createDefaultPaintArray();
 
 		for (XYSeriesCollection xySeriesCollection : xyDataSetList) 
 		{
-			JFreeChart xyChart = buildChartFromSeries( xySeriesCollection,  yTitle, displayLabels, option); 
-			yTitle = null;  			// used only once
-			displayLabels = false;		// used only once
-			xyChart.setID (Integer.toString(ichart));
+			NumberAxis domainAxis = new NumberAxis("Cage " + icage);
+			XYLineAndShapeRenderer subPlotRenderer = new XYLineAndShapeRenderer(true, false);
+			final XYPlot subplot = new XYPlot(xySeriesCollection, domainAxis, null, subPlotRenderer);
+			int icolor = 0;
+			for (int i=0; i< xySeriesCollection.getSeriesCount(); i++, icolor++ )
+			{
+				if (icolor > color.length)
+					icolor = 0;
+				subPlotRenderer.setSeriesPaint(i, color[icolor]);
+			}
+			
+			subplot.setBackgroundPaint(Color.LIGHT_GRAY);
+			subplot.setDomainGridlinePaint(Color.WHITE);
+			subplot.setRangeGridlinePaint(Color.WHITE);
+			parent.add(subplot);
 						
-			xyChartList.add(xyChart);
-			ChartPanel xyChartPanel = new ChartPanel(xyChart, width, height, minimumDrawWidth, 100, 
-					maximumDrawWidth, maximumDrawHeight, false, false, true, true, true, true);
-			xyChartPanel.addChartMouseListener(new ChartMouseListener() {
-			    public void chartMouseClicked(ChartMouseEvent e) {selectKymoImage(getSelectedCurve(e)); }
-			    public void chartMouseMoved(ChartMouseEvent e) {}
-			});
-			mainChartPanel.add(xyChartPanel);
-			ichart += xySeriesCollection.getSeriesCount();
-
-			width = 100;
-			height = 200;
-			minimumDrawWidth = 50;
-			maximumDrawWidth = width;
-			maximumDrawHeight = 200;
-
+			icage++;
 		}
+		
+        JFreeChart chart = new JFreeChart("Capillary levels", null, parent, true);
+
+        final ChartPanel panel = new ChartPanel(chart, true, true, true, false, true);
+        panel.addChartMouseListener(new ChartMouseListener() {
+		    public void chartMouseClicked(ChartMouseEvent e) {selectKymoImage(getSelectedCurve(e)); }
+		    public void chartMouseMoved(ChartMouseEvent e) {}
+		});
+    	
+		mainChartPanel.add(panel);
 		mainChartFrame.pack();
 		mainChartFrame.setLocation(pt);
 		mainChartFrame.addToDesktopPane ();
 		mainChartFrame.setVisible(true);
 	}
-	
 	
 	private int getSelectedCurve(ChartMouseEvent e) 
 	{
@@ -131,46 +139,28 @@ public class ChartLevels extends IcyFrame
         	return -1;
         		
 		JFreeChart chart = e.getChart();
-        int isel = Integer.valueOf(chart.getID());
-        
+//        int isel = Integer.valueOf(chart.getID());
 		ChartEntity chartEntity = e.getEntity();
-		if (chartEntity != null && chartEntity instanceof XYItemEntity) {
-            XYItemEntity ent = (XYItemEntity) chartEntity;
-            isel += ent.getSeriesIndex();
-		}
+		// TODO: use handleclick? jfreechart CombinedRangeXYPlot handleClick
+		int isel= -1;
+//		if (chartEntity != null && chartEntity instanceof XYItemEntity) {
+//            XYItemEntity ent = (XYItemEntity) chartEntity;
+//            isel += ent.getSeriesIndex();
+//		}
+		
+		
+		System.out.println( " entity" + chartEntity);
 		return isel;
 	}
-	
+
 	private void selectKymoImage(int isel)
 	{
 		Experiment exp = (Experiment) parent0.expListCombo.getSelectedItem();
         Viewer v = exp.seqKymos.seq.getFirstViewer();
         v.setPositionT(isel);
 	}
-	
-	private JFreeChart buildChartFromSeries(XYSeriesCollection xyDataset, String yTitle, boolean displayLabels, EnumXLSExportType option) 
-	{
-		JFreeChart xyChart = ChartFactory.createXYLineChart(
-				null,				// chartname 
-				null, 				// xDomain
-				yTitle, 			// yDomain
-				xyDataset, 			// collection
-				PlotOrientation.VERTICAL, 
-				true, 				// legend
-				false, 				// tooltips
-				false);				// url
-		xyChart.setAntiAlias( true );
-		xyChart.setTextAntiAlias( true );
-		
-		setYAxis(xyChart, displayLabels);
-		setXAxis(xyChart);
-		if (option == EnumXLSExportType.TOPLEVEL || option == EnumXLSExportType.BOTTOMLEVEL) 
-			xyChart.getXYPlot().getRangeAxis(0).setInverted(true);
-		
-		return xyChart;
-	}
 
-	private void getDataArrays(Experiment exp, EnumXLSExportType exportType, boolean subtractEvaporation, List<XYSeriesCollection> xyList) 
+	private List<XYSeriesCollection> getDataArrays(Experiment exp, EnumXLSExportType exportType, boolean subtractEvaporation) 
 	{
 		XLSResultsArray resultsArray1 = getDataAsResultsArray(exp, exportType, subtractEvaporation);
 		XLSResultsArray resultsArray2 = null;
@@ -180,6 +170,7 @@ public class ChartLevels extends IcyFrame
 		XYSeriesCollection xyDataset = null;
 		int oldcage = -1;
 		
+		List<XYSeriesCollection> xyList = new ArrayList<XYSeriesCollection>();
 		for (int iRow = 0; iRow < resultsArray1.size(); iRow++ ) 
 		{
 			XLSResults rowXLSResults1 = resultsArray1.getRow(iRow);
@@ -197,6 +188,7 @@ public class ChartLevels extends IcyFrame
 			xyDataset.addSeries(seriesXY );
 			updateGlobalMaxMin();
 		}
+		return xyList;
 	}
 	
 	private XLSResultsArray getDataAsResultsArray(Experiment exp, EnumXLSExportType exportType, boolean subtractEvaporation)
@@ -208,26 +200,6 @@ public class ChartLevels extends IcyFrame
 		
 		XLSExport xlsExport = new XLSExport();
 		return xlsExport.getCapDataFromOneExperiment(exp, exportType, options);
-	}
-	
-	private void setXAxis(JFreeChart xyChart) {
-
-	if( parent0.paneExcel.tabCommonOptions.getIsFixedFrame())
-	{
-		double binMs	= parent0.paneExcel.tabCommonOptions.getBinMs();
-		globalXMin 		= parent0.paneExcel.tabCommonOptions.getStartMs()/binMs;
-		globalXMax 		= parent0.paneExcel.tabCommonOptions.getEndMs()/binMs;
-	}
-	ValueAxis xAxis = xyChart.getXYPlot().getDomainAxis(0);
-	xAxis.setRange(globalXMin, globalXMax);
-	}
-	
-	private void setYAxis(JFreeChart xyChart, boolean displayLabels) {
-		ValueAxis yAxis = xyChart.getXYPlot().getRangeAxis(0);
-		if (globalYMin == globalYMax)
-			globalYMax = globalYMin +1;
-		yAxis.setRange(globalYMin, globalYMax);
-		yAxis.setTickLabelsVisible(displayLabels);
 	}
 
 	private void updateGlobalMaxMin() 
@@ -282,4 +254,6 @@ public class ChartLevels extends IcyFrame
 			x++;
 		}
 	}
+
+	
 }
