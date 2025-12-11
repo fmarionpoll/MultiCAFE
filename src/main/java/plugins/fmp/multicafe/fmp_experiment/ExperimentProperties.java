f48dd4b5ca70cf31d4601c26b282408ce628ee49
Frederic Marion-Poll <marion_poll@hotmail.com>
2025-12-11 17:24:56 +0100

    add backward compatibility to xml files

--- src/main/java/plugins/fmp/multicafe/fmp_experiment/ExperimentProperties.java
+++ src/main/java/plugins/fmp/multicafe/fmp_experiment/ExperimentProperties.java
@@ -14,51 +14,87 @@ public class ExperimentProperties {
 	public String field_experiment = new String("..");
 	public String field_stim1 = new String("..");
 	public String field_conc1 = new String("..");
-	public String field_comment1 = new String("..");
-	public String field_comment2 = new String("..");
-	public String field_strain = new String("..");
-	public String field_sex = new String("..");
 	public String field_stim2 = new String("..");
 	public String field_conc2 = new String("..");
+	public String field_string1 = new String("..");
+	public String field_string2 = new String("..");
 
+	// Legacy fields kept for backward compatibility (read but not written in new
+	// format)
+	public String field_comment1 = new String("..");
+	public String field_comment2 = new String("..");
+
+	public String field_strain = new String("..");
+	public String field_sex = new String("..");
+
+	// New field names (human-readable)
+	private final static String ID_STIM1_NEW = "stim1";
+	private final static String ID_CONC1_NEW = "conc1";
+	private final static String ID_STIM2_NEW = "stim2";
+	private final static String ID_CONC2_NEW = "conc2";
+	private final static String ID_STRING1 = "string1";
+	private final static String ID_STRING2 = "string2";
+
+	// Legacy field names (for backward compatibility)
 	private final static String ID_BOXID = "boxID";
 	private final static String ID_EXPERIMENT = "experiment";
-	private final static String ID_STIM1 = "stim";
-	private final static String ID_CONC1 = "conc";
-
-	private final static String ID_COMMENT1 = "comment";
-	private final static String ID_COMMENT2 = "comment2";
+	private final static String ID_STIM1_OLD = "comment"; // MS96Experiment.xml format
+	private final static String ID_CONC1_OLD = "comment2"; // MS96Experiment.xml format
+	private final static String ID_STIM2_OLD = "cond1"; // MS96Experiment.xml format
+	private final static String ID_CONC2_OLD = "cond2"; // MS96Experiment.xml format
 	private final static String ID_STRAIN = "strain";
 	private final static String ID_SEX = "sex";
-	private final static String ID_STIM2 = "cond1";
-	private final static String ID_CONC2 = "cond2";
 
 	public void saveXML_Properties(Node node) {
 		XMLUtil.setElementValue(node, ID_BOXID, field_boxID);
 		XMLUtil.setElementValue(node, ID_EXPERIMENT, field_experiment);
-		XMLUtil.setElementValue(node, ID_STIM1, field_stim1);
-		XMLUtil.setElementValue(node, ID_CONC1, field_conc1);
 
-		XMLUtil.setElementValue(node, ID_COMMENT1, field_comment1);
-		XMLUtil.setElementValue(node, ID_COMMENT2, field_comment2);
+		// Save new human-readable field names
+		XMLUtil.setElementValue(node, ID_STIM1_NEW, field_stim1);
+		XMLUtil.setElementValue(node, ID_CONC1_NEW, field_conc1);
+		XMLUtil.setElementValue(node, ID_STIM2_NEW, field_stim2);
+		XMLUtil.setElementValue(node, ID_CONC2_NEW, field_conc2);
+		XMLUtil.setElementValue(node, ID_STRING1, field_string1);
+		XMLUtil.setElementValue(node, ID_STRING2, field_string2);
+
+		// Save other fields
 		XMLUtil.setElementValue(node, ID_STRAIN, field_strain);
 		XMLUtil.setElementValue(node, ID_SEX, field_sex);
-		XMLUtil.setElementValue(node, ID_STIM2, field_stim2);
-		XMLUtil.setElementValue(node, ID_CONC2, field_conc2);
+
+		// Note: We don't save old comment1/comment2 fields in new format
+		// to avoid confusion with the new field structure
 	}
 
 	public void loadXML_Properties(Node node) {
 		field_boxID = XMLUtil.getElementValue(node, ID_BOXID, "..");
 		field_experiment = XMLUtil.getElementValue(node, ID_EXPERIMENT, "..");
-		field_stim1 = XMLUtil.getElementValue(node, ID_STIM1, "..");
-		field_conc1 = XMLUtil.getElementValue(node, ID_CONC1, "..");
 
-		field_comment1 = XMLUtil.getElementValue(node, ID_COMMENT1, "..");
-		field_comment2 = XMLUtil.getElementValue(node, ID_COMMENT2, "..");
+		// Load new fields first (if present in new format)
+		// Check if new format fields exist by testing if element is present
+		org.w3c.dom.Node stim1Node = XMLUtil.getElement(node, ID_STIM1_NEW);
+		if (stim1Node != null) {
+			// New format: use new field names
+			field_stim1 = XMLUtil.getElementValue(node, ID_STIM1_NEW, "..");
+			field_conc1 = XMLUtil.getElementValue(node, ID_CONC1_NEW, "..");
+			field_stim2 = XMLUtil.getElementValue(node, ID_STIM2_NEW, "..");
+			field_conc2 = XMLUtil.getElementValue(node, ID_CONC2_NEW, "..");
+			field_string1 = XMLUtil.getElementValue(node, ID_STRING1, "..");
+			field_string2 = XMLUtil.getElementValue(node, ID_STRING2, "..");
+		} else {
+			// Old format: try to load from legacy field names
+			// Try MS96Experiment.xml format first (stim/conc/cond1/cond2)
+			org.w3c.dom.Node stimOldNode = XMLUtil.getElement(node, ID_STIM1_OLD);
+			if (stimOldNode != null) {
+				// MS96Experiment.xml format
+				field_stim1 = XMLUtil.getElementValue(node, ID_STIM1_OLD, "..");
+				field_conc1 = XMLUtil.getElementValue(node, ID_CONC1_OLD, "..");
+				field_stim2 = XMLUtil.getElementValue(node, ID_STIM2_OLD, "..");
+				field_conc2 = XMLUtil.getElementValue(node, ID_CONC2_OLD, "..");
+			}
+		}
+
 		field_strain = XMLUtil.getElementValue(node, ID_STRAIN, "..");
 		field_sex = XMLUtil.getElementValue(node, ID_SEX, "..");
-		field_stim2 = XMLUtil.getElementValue(node, ID_STIM2, "..");
-		field_conc2 = XMLUtil.getElementValue(node, ID_CONC2, "..");
 	}
 
 	public String getField(EnumXLSColumnHeader fieldEnumCode) {
@@ -161,8 +197,8 @@ public class ExperimentProperties {
 	public String csvExportSectionHeader(String csvSep) {
 		StringBuffer sbf = new StringBuffer();
 		sbf.append("#" + csvSep + "DESCRIPTION" + csvSep + "multiSPOTS96 data\n");
-		List<String> row2 = Arrays.asList(ID_BOXID, ID_EXPERIMENT, ID_STIM1, ID_CONC1, ID_COMMENT1, ID_COMMENT2,
-				ID_STRAIN, ID_SEX, ID_STIM2, ID_CONC2);
+		List<String> row2 = Arrays.asList(ID_BOXID, ID_EXPERIMENT, ID_STIM1_NEW, ID_CONC1_NEW, ID_STRING1, ID_STRING2,
+				ID_STRAIN, ID_SEX, ID_STIM2_NEW, ID_CONC2_NEW);
 		sbf.append(String.join(csvSep, row2));
 		sbf.append("\n");
 		return sbf.toString();
@@ -170,8 +206,8 @@ public class ExperimentProperties {
 
 	public String csvExportProperties(String csvSep) {
 		StringBuffer sbf = new StringBuffer();
-		List<String> row3 = Arrays.asList(field_boxID, field_experiment, field_stim1, field_conc1, field_comment1,
-				field_comment2, field_strain, field_sex, field_stim2, field_conc2);
+		List<String> row3 = Arrays.asList(field_boxID, field_experiment, field_stim1, field_conc1, field_string1,
+				field_string2, field_strain, field_sex, field_stim2, field_conc2);
 		sbf.append(String.join(csvSep, row3));
 		sbf.append("\n");
 		return sbf.toString();
@@ -187,17 +223,22 @@ public class ExperimentProperties {
 		i++;
 		field_conc1 = data[i];
 		i++;
-		field_comment1 = data[i];
+		field_string1 = (data.length > i) ? data[i] : "..";
 		i++;
-		field_comment2 = data[i];
+		field_string2 = (data.length > i) ? data[i] : "..";
 		i++;
-		field_strain = data[i];
+		field_strain = (data.length > i) ? data[i] : "..";
 		i++;
-		field_sex = data[i];
+		field_sex = (data.length > i) ? data[i] : "..";
 		i++;
-		field_stim2 = data[i];
+		field_stim2 = (data.length > i) ? data[i] : "..";
 		i++;
-		field_conc2 = data[i];
+		field_conc2 = (data.length > i) ? data[i] : "..";
+		// Legacy fields: try to preserve if present in old format
+		if (data.length > i + 1) {
+			field_comment1 = data[i];
+			field_comment2 = data[i + 1];
+		}
 	}
 
 	// ================ getters / setters
@@ -240,5 +281,13 @@ public class ExperimentProperties {
 
 	public String getField_conc2() {
 		return field_conc2;
+	}
+
+	public String getField_string1() {
+		return field_string1;
+	}
+
+	public String getField_string2() {
+		return field_string2;
 	}
 }
